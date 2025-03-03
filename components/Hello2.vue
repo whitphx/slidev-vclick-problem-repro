0cdb7e19331918b65e12936647fa30e012e87974
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-03-03 17:52:46 +0800

    Add another example

---
+++ components/Hello2.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    Hello2
+  </div>
+  <!-- The bug occurs even when the comment is placed here, after the element. -->
+</template>
--- slides.md
+++ slides.md
@@ -3,3 +3,5 @@ title: Repro v-click bug in the build mode
 ---
 
 <Hello v-click />
+
+<Hello2 v-click />
