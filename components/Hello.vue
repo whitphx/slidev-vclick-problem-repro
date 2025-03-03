1636e5102a06209cd3c531aa6fedf10dd2a90c8f
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-03-03 18:09:51 +0800

    Fix

--- components/Hello.vue
+++ components/Hello.vue
@@ -1,6 +1,4 @@
 <template>
   <!-- This top level comment causes the bug where the v-click on this component doesn't work in the build mode -->
-  <div>
-    Hello
-  </div>
+  <div>Hello</div>
 </template>
--- components/Hello2.vue
+++ components/Hello2.vue
@@ -1,6 +1,4 @@
 <template>
-  <div>
-    Hello2
-  </div>
+  <div>Hello2</div>
   <!-- The bug occurs even when the comment is placed here, after the element. -->
 </template>
